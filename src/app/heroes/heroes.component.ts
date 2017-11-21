2cbd42ecd28bd16c02de38a2e1f89b15ecaf5cae
0x0 <ghstoreinvesting@gmail.com>
2017-11-21 15:46:49 +0800

    finish adding routers

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,21 +9,11 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
-
   heroes: Hero[];
-  selectedHero: Hero;
 
   getHeroes() {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
-  }
-
-  onSelect(hero) {
-    this.selectedHero = hero;
   }
 
   constructor(private heroService: HeroService) { }
