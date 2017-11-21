c7807dd4584ae14c5f2021e54127b737e60b70b5
0x0 <ghstoreinvesting@gmail.com>
2017-11-21 21:45:38 +0800

    Revert "add to revert"
    This reverts commit 08b3a42983ef896685410c2253697cbaa9ac61dd.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,22 +4,18 @@ import { HEROES } from './mock-heroes';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of';
 import { MessageService } from './message.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable()
 export class HeroService {
-  private heroesURL: string = 'https://raw.githubusercontent.com/noobling/angular-tour-heroes/master/src/app/heroes.json';
-
   getHeroes(): Observable<Hero[]> {
     this.messageService.add('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesURL);
+    return of(HEROES);
   }
 
   getHero(id: number): Observable<Hero> {
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
   }
-  constructor(private messageService: MessageService, 
-              private http: HttpClient) { }
+  constructor(private messageService: MessageService) { }
 
 }
